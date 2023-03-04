f469433a2f28e8b72ba28c4b0f5471f5551d2b14
Andrés Rengifo <andresm.rengifo@gmail.com>
2023-03-04 13:20:46 -0500

    se crea ejemplos de listas, for y while

---
+++ lists.py
@@ -0,0 +1,3 @@
+numeros = [1,2,3,4,5,6];
+for numerito in numeros:
+    print(numerito)
---
+++ while.py
@@ -0,0 +1,20 @@
+controladora  = 100;
+print('***Empanadas el machetico***') 
+print('****************************') 
+print('1. Agregar sabor empanada') 
+print('2. Mostrar sabot empanada') 
+print('3. Cambiar sabor empanada') 
+print('0. Salir') 
+print('****************************') 
+while (controladora != 0):
+    controladora = int(input('Digita una opcion: '));
+    if controladora == 1:
+        pass
+    elif controladora == 2:
+        pass
+    elif controladora == 3:
+        pass
+    elif controladora == 0:
+        pass
+    else:
+        pass
